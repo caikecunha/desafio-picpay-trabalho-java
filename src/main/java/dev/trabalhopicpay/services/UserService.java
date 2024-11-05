9d9ec0df6b7028417ae97089c6b7572c69e457cd
Caike Cunha <119429343+caikecunha@users.noreply.github.com>
2024-11-05 16:07:05 -0300

    Update UserService.java

--- src/main/java/dev/trabalhopicpay/services/UserService.java
+++ src/main/java/dev/trabalhopicpay/services/UserService.java
@@ -11,24 +11,17 @@ import java.util.List;
 @ApplicationScoped
 public class UserService {
 
-    // Vem de um Controller @GET
-    // Lista toda a lista de usuários
+    // Lista de usuários
     public List<User> listUsers() {
         return User.listAll();
     }
 
-    // Vem de um Controller @POST
-    // Persist (Cria usuário) no banco de dados
-    // Transactional é usado, pois o methode altera dados no banco de dados
     @Transactional
     public User saveUser(User user) {
         user.persist();
         return user;
     }
 
-    // Regra de negócia que valida se a transferência pode ser realizada
-    // Se o usuário remetente for um de conta logista, uma Exception é lançada
-    // Se o usuário remetente não possuir saldo para transferência, uma Exception é lançada
     public void validadeTransaction(User sender, BigDecimal amount) throws Exception {
         if(sender.userType == UserType.MERCHANT) {
             throw new Exception("Lojistas não podem realizar transferências.");
@@ -38,7 +31,6 @@ public class UserService {
         }
     }
 
-    // Regra de negócio que busca usuário remetente no banco, uma Exception é lançada se o usuário não existir
     public User findUserSenderById(Long id) throws Exception {
         if (User.findById(id) == null) {
             throw new Exception("Usuário remetente não encontrado");
@@ -46,7 +38,6 @@ public class UserService {
         return User.findById(id);
     }
 
-    // Regra de negócio que busca usuário Destinatário no banco, uma Exception é lançada se o usuário não existir
     public User findUserReceiverById(Long id) throws Exception {
         if (User.findById(id) == null) {
             throw new Exception("Usuário destinatário não encontrado");
